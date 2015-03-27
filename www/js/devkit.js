080149ac5081965fb1a73615b238399561707c99
Emile Nijssen <emilenijssen@gmail.com>
2015-03-28 00:58:08 +0100

    no message

--- www/js/devkit.js
+++ www/js/devkit.js
@@ -1,8 +1,22 @@
+window.onload = function(){
+	
+	
+	var editor = CodeMirror(document.getElementById('codemirror'), {
+		value: 	"function myScript(){\n\treturn 100;\n}\n",
+		mode:  	"javascript",
+		theme: 	"monokai",
+		lineNumbers: true
+	});
+	
+	
+}
+
 var app = angular.module('devkit', []);
 
 app.controller("appCtrl", function($scope, $rootScope) {
 	
 	$scope.project = {
+		id: 'nl.athom.hello',
 		name: 'Hello World'
 	}
 	
@@ -12,16 +26,44 @@ app.controller("sidebarCtrl", function($scope, $rootScope) {
 	
 	$scope.items = [
 		{
-			name: 'assets',
+			name: 'nl.athom.hello',
 			type: 'folder',
-			childs: [
+			children: [
 				{
 					name: 'assets',
-					type: 'folder'
+					type: 'folder',
+					children: [
+						{
+							name: 'icon.svg',
+							type: 'file'
+						}
+						
+					]
+				},
+				{
+					name: 'animations',
+					type: 'folder',
+					children: [
+						{
+							name: 'rainbow.js',
+							type: 'file'
+						}
+						
+					]
 				}
-				
 			]
 		}
 	]
 	
 });
+
+app.directive('stopEvent', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attr) {
+            element.bind('click', function (e) {
+                e.stopPropagation();
+            });
+        }
+    };
+ });
