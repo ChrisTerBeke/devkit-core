f5612347879b3f097532316eb8fa478dbd115ce1
Emile Nijssen <emilenijssen@gmail.com>
2015-03-29 19:29:48 +0200

    no message

--- www/js/devkit.js
+++ www/js/devkit.js
@@ -1,20 +1,91 @@
+var os 			= require('os');
 var fs 			= require('fs');
 var path 		= require('path');
 
 var watchTree = require("fs-watch-tree").watchTree;
 
-require('nw.gui').Window.get().showDevTools()
+//require('nw.gui').Window.get().showDevTools()
 
-var app = angular.module('devkit', ['ui.codemirror']);
+var app = angular.module('devkit', ['ui.codemirror', 'cfp.hotkeys', 'ngTagsInput']);
 
-app.controller("devkitCtrl", function($scope, $rootScope) {
+var keycombos = [ 'ctrl+s', 'ctrl+w' ];
+
+app.controller("devkitCtrl", function($scope, $rootScope, $http, hotkeys) {
 	
 	$rootScope.project = {
 		path: '/Users/emile/GitHub/nl.athom.hello/',
 		metadata: {
-			id: 'nl.athom.hello',
-			name: 'Hello World'
+			"id": "nl.athom.hello",
+			"version": "1.0.0",
+			"name": {
+				"en": "Hello World",
+				"nl": "Hallo wereld"
+			},
+			"description": {
+				"en": "Example app for Homey"
+			},
+			"author": {
+				"name": "Emile Nijssen",
+				"website": "http://www.emilenijssen.nl"
+			},
+			"dependencies": {
+				"socket.io": "^1.1.0"
+			},
+			"permissions": [
+				"homey:manager:speech-input"
+			],
+			"interfaces": {
+				"speech": {
+					"triggers": [
+						{
+							"id": "hello",
+							"importance": 0.6,
+							"synonyms": {
+								"en": [ "hello", "hi", "hey" ],
+								"nl": [ "hallo", "hai", "hoi", "hey" ]
+							}
+						}
+					]
+				}
+			}
 		}
+	}
+		
+	$scope.run = function(){
+		alert('run')
+	}
+	
+	$scope.platform = os.platform();
+	
+	// progressbar
+	$scope.progressbar = 0;
+	
+    $rootScope.$on('progressbar', function( event, percent ) {
+	    console.log(percent)
+		$scope.progressbar = percent;
+    });
+	
+	// bind hotkeys
+	keycombos.forEach(function(combo){
+		
+		var combo_ = combo;
+		
+		// replace CTRL by CMD on OSX
+		if( os.platform() == 'darwin' ) {
+			combo_ = combo_.replace('ctrl', 'meta');
+		}
+				
+		hotkeys.add({
+			combo: combo_,
+			callback: function(){
+				$rootScope.$emit('keycombo.' + combo);
+			}
+		});
+		
+	});
+	
+	$scope.autocompletePermissionTags = function( query ){
+		return $http.get('permissions.json');
 	}
 	
 });
@@ -72,29 +143,41 @@ function readdirSyncRecursive( dir, root ) {
 
 app.controller("sidebarCtrl", function($scope, $rootScope) {
 	
-	// watch for any changes in the directory
-	$scope.filetree = readdirSyncRecursive( $rootScope.project.path, true );
-
-	var watch = watchTree($rootScope.project.path, function (event) {
-		$scope.editor.filetree = readdirSyncRecursive( $rootScope.project.path, true );
-		$scope.$apply();
-	});
-	
 	// open a new file on sidebar click
 	$scope.open = function( path ){
 		$rootScope.$emit('editor.open', path);
 	}
 	
+	$scope.update = function(){
+		$scope.filetree = readdirSyncRecursive( $rootScope.project.path, true );
+	}
+
+	$scope.update();
+		
+	// watch for any changes in the directory
+	var watch = watchTree($rootScope.project.path, function (event) {
+		$scope.update();
+		$scope.$apply();
+	});
+	
 });
 
 app.controller("editorCtrl", function($scope, $rootScope) {
     
-	$scope.files = []; // files open
-	$scope.active = ''; // currently viewing
+	$scope.files = {}; // files open
+	$scope.active = undefined; // currently viewing
 	
 	// open a new file
     $rootScope.$on('editor.open', function( event, file_path ) {
 		$scope.open( file_path )
+    });
+    
+    $rootScope.$on('keycombo.ctrl+s', function(){
+		$scope.save();
+    });
+    
+    $rootScope.$on('keycombo.ctrl+w', function(){
+		$scope.close();
     });
 	
 	// codemirror related
@@ -109,35 +192,31 @@ app.controller("editorCtrl", function($scope, $rootScope) {
 			// fix hidden bug
 			setTimeout( function(){
 				_editor.refresh();
-			   _editor.focus();
+				_editor.focus();
 			}, 1 );
 			
 			// Events
 			_editor.on("change", function( _editor, changeObj ){
 				console.log('change', changeObj)
+				
+				$scope.files[ $scope.active ]._changed = true;
 			});
 		}
 	};
 	    
     $scope.open = function( file_path ){
 	    
-	    // check if the file is already open
-	    var already_open = false;
-	    $scope.files.forEach(function( file ){
-			if( file.path == file_path ) {
-				already_open = true;
-			}
-	    });
-	    
-	    if( !already_open ) {
+	    // add the file if it's not already open
+	    if( typeof $scope.files[ file_path ] == 'undefined' ) {
 		    		    
 		    // create a file entry
-		    $scope.files.push({
+		    $scope.files[ file_path ] = {
 			    name: path.basename( file_path ),
 			    icon: '', // todo
 			    path: file_path,
-			    code: fs.readFileSync( file_path ).toString()
-		    });	    
+			    code: fs.readFileSync( file_path ).toString(),
+			    _changed: false
+		    } 
 	    }
 	    
 	    $scope.active = file_path;
@@ -146,16 +225,38 @@ app.controller("editorCtrl", function($scope, $rootScope) {
     // close an item
     $scope.close = function( file_path ) {
 	    
-	    // TODO check for unsaved changes
+	    if( typeof $scope.active == 'undefined' ) return;
 	    
-		$scope.files = $scope.files.filter(function( file ){
-			if( file.path == file_path ) return false;
-			return true;
-		});
+	    var activeFile = $scope.files[ $scope.active ];
+	    
+	    // check for unsaved changes
+	    if( activeFile._changed ) {
+		    if( confirm("There are unsaved changes, close " + activeFile.name + " anyway?" ) ) {
+			    delete $scope.files[ $scope.active ];
+		    }
+	    } else {
+		    delete $scope.files[ $scope.active ];
+	    }
 		
 		// set last tab as active
 		// TODO: set last viewed tab as active
-		$scope.active = $scope.files[ $scope.files.length-1 ].path;
+		if( Object.keys($scope.files).length > 0 ) {			
+			$scope.active = $scope.files[Object.keys($scope.files)[Object.keys($scope.files).length - 1]].path;
+		}
+    }
+    
+    $scope.save = function(){
+	    if( typeof $scope.active == 'undefined' ) return;
+	    
+	    $rootScope.$emit('progressbar', 0);
+	    
+	    var activeFile = $scope.files[ $scope.active ];
+	    
+	    fs.writeFileSync( activeFile.path, activeFile.code );
+	    
+	    $rootScope.$emit('progressbar', 1);
+				
+		activeFile._changed = false;
     }
     
     $scope.view = function( file_path ) {
@@ -173,13 +274,6 @@ app.controller("editorCtrl", function($scope, $rootScope) {
     
 });
 
-app.controller("fileCtrl", function($scope, $rootScope) {
-	
-	// create codemirror instance
-	$scope.code = 'alert("hoi!")';
-	
-});
-
 app.directive('stopEvent', function () {
     return {
         restrict: 'A',
@@ -189,4 +283,4 @@ app.directive('stopEvent', function () {
             });
         }
     };
- });
+});
