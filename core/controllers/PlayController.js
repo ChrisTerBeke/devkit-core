dfaed6dca6cca47f75ef58e527b91d2a06a3098e
Chris ter Beke <ctbeke@gmail.com>
2015-04-24 14:25:57 +0200

    fix for play button

--- core/controllers/PlayController.js
+++ core/controllers/PlayController.js
@@ -4,18 +4,18 @@ app.controller("playCtrl", function($scope, $rootScope, $filter)
 	$scope.shouldBeEnabled;
 
 	$scope.playstop = function(){
-		$rootScope.emit('play.playstop', $scope.status); // listen to this in your custom play controller
+		$rootScope.$emit('play.playstop', $scope.status); // listen to this in your custom play controller
 	}
 
-	$rootScope.on('play.enable', function() { // listen to enable event (trigger in your custom play controller
+	$rootScope.$on('play.enable', function() { // listen to enable event (trigger in your custom play controller
 		$scope.shouldBeEnabled = true;
 	});
 
-	$rootScope.on('play.disable', function() { // listen to disable event (trigger in your custom play controller
+	$rootScope.$on('play.disable', function() { // listen to disable event (trigger in your custom play controller
 		$scope.shouldBeEnabled = false;
 	});
 
-	$rootScope.on('play.status', function(status) { // listen to status event (trigger in your custom play controller
+	$rootScope.$on('play.status', function(status) { // listen to status event (trigger in your custom play controller
 		$scope.status = status;
 	});
 });
