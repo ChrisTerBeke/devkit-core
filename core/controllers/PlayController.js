2b99b0cfcb031a67224b094a4a6ea6f323963fd9
Chris ter Beke <ctbeke@gmail.com>
2015-04-24 14:23:24 +0200

    global play events, controller to control play button

---
+++ core/controllers/PlayController.js
@@ -0,0 +1,21 @@
+app.controller("playCtrl", function($scope, $rootScope, $filter)
+{
+	$scope.status = 'idle';
+	$scope.shouldBeEnabled;
+
+	$scope.playstop = function(){
+		$rootScope.emit('play.playstop', $scope.status); // listen to this in your custom play controller
+	}
+
+	$rootScope.on('play.enable', function() { // listen to enable event (trigger in your custom play controller
+		$scope.shouldBeEnabled = true;
+	});
+
+	$rootScope.on('play.disable', function() { // listen to disable event (trigger in your custom play controller
+		$scope.shouldBeEnabled = false;
+	});
+
+	$rootScope.on('play.status', function(status) { // listen to status event (trigger in your custom play controller
+		$scope.status = status;
+	});
+});
