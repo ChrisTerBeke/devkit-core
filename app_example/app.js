820e50105a46a2802244eacdae23af3eb1978721
Chris ter Beke <ctbeke@gmail.com>
2015-04-25 19:25:44 +0200

    add default widgets and load them in app_example

--- app_example/app.js
+++ app_example/app.js
@@ -1,28 +1,64 @@
 app.run(['$rootScope', '$timeout', '$play', '$ocLazyLoad', '$file', function($rootScope, $timeout, $play, $ocLazyLoad, $file) {
 	$timeout(function() {
 
+		// lazy load markdown widget
+		$ocLazyLoad.load('markdown');
+
+		// lazy load codemirror
+		$ocLazyLoad.load('devkit-editor-codemirror');
+
 		$file.setConfig([
 			{
 				ext: ".js",
 				dir: "/animations",
 				config: {
 					view: "codemirror",
-					widgets: [ 'ledring' ]
+					widgets: [ 'markdown' ]
 				}
 			}
-		]);
-
-		// lazy load codemirror
-		$ocLazyLoad.load([
-			'./bower_components/devkit-editor-codemirror/js/codemirror/lib/codemirror.js',
-			'./bower_components/devkit-editor-codemirror/js/codemirror/mode/javascript/javascript.js',
-			'./bower_components/devkit-editor-codemirror/js/angular-ui-codemirror/ui-codemirror.js',
-			'./bower_components/devkit-editor-codemirror/js/CodemirrorController.js',
-			'./bower_components/devkit-editor-codemirror/css/codemirror.css',
-			'./bower_components/devkit-editor-codemirror/codemirror.html'
 		]);
 
 		$play.status('loading...');
 
 	}, 100);
 }]);
+
+app.config(function($ocLazyLoadProvider) {
+
+	$ocLazyLoadProvider.config({
+		debug: true,
+		modules: [
+			{
+				name: 'markdown',
+				serie: true,
+				files: [
+					'./widgets/markdown/MarkdownController.js',
+					'./widgets/markdown/markdown.css',
+					'./widgets/markdown/markdown.html'
+				]
+			},
+			{
+				name: 'svg',
+				serie: true,
+				files: [
+					'./widgets/svg/SvgController.js',
+					'./widgets/svg/svg.css',
+					'./widgets/svg/svg.html'
+				]
+			},
+			{
+				name: 'devkit-editor-codemirror',
+				serie: true,
+				files: [
+					'./bower_components/devkit-editor-codemirror/js/codemirror/lib/codemirror.js',
+					'./bower_components/devkit-editor-codemirror/js/codemirror/mode/javascript/javascript.js',
+					'./bower_components/devkit-editor-codemirror/js/angular-ui-codemirror/ui-codemirror.js',
+					'./bower_components/devkit-editor-codemirror/js/CodemirrorController.js',
+					'./bower_components/devkit-editor-codemirror/css/codemirror.css',
+					'./bower_components/devkit-editor-codemirror/codemirror.html'
+				]
+			}
+		]
+	});
+
+});
