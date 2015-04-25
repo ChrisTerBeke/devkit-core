f4a19aff682c3bb78dda763931be2be61448b41f
Chris ter Beke <ctbeke@gmail.com>
2015-04-25 20:24:51 +0200

    always load markdown and svg widgets in core

--- core/app.js
+++ core/app.js
@@ -14,8 +14,37 @@ app.config(function($sceDelegateProvider) {
 	$sceDelegateProvider.resourceUrlWhitelist(window.AUTH.whitelist);
 });
 
+app.config(function($ocLazyLoadProvider) {
+
+	$ocLazyLoadProvider.config({
+		debug: true,
+		modules: [
+			{
+				name: 'markdown',
+				serie: true,
+				files: [
+					'./widgets/markdown/MarkdownController.js',
+					'./widgets/markdown/markdown.css',
+					'./widgets/markdown/markdown.html'
+				]
+			},
+			{
+				name: 'svg',
+				serie: true,
+				files: [
+					'./widgets/svg/SvgController.js',
+					'./widgets/svg/svg.css',
+					'./widgets/svg/svg.html'
+				]
+			}
+		]
+	});
+
+});
+
 // add Bearer token to $http requests
-app.run(['$rootScope', '$injector', function($rootScope, $injector) {
+app.run(['$rootScope', '$injector', '$ocLazyLoad', function($rootScope, $injector, $ocLazyLoad) {
+
     $injector.get("$http").defaults.transformRequest = function(data, headersGetter) {
         if ($rootScope.user) {
         	headersGetter()['Authorization'] = "Bearer " + window.localStorage.access_token;
@@ -24,6 +53,12 @@ app.run(['$rootScope', '$injector', function($rootScope, $injector) {
             return angular.toJson(data);
         }
     };
+
+    // lazy load markdown widget
+	$ocLazyLoad.load('markdown');
+
+	// lazy load svg widget
+	$ocLazyLoad.load('svg');
 }]);
 
 if(typeof angular !== 'undefined' && window.DEBUG) {
