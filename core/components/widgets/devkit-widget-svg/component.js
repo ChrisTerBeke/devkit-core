5b00c385e11139b1204a900d71d89ad2e3d9e7d7
ChrisTerBeke <ctbeke@gmail.com>
2015-05-05 20:46:50 +0200

    svg cleanup and default styling

--- core/components/widgets/devkit-widget-svg/component.js
+++ core/components/widgets/devkit-widget-svg/component.js
@@ -3,8 +3,6 @@ app
 
 	$scope.src = $scope.file_path;
 
-	console.log('active scope', $scope.$parent.active);
-
 	$rootScope.$on('editor.saved.' + $scope.file_path, function(){
 		$scope.src = $scope.file_path + '?rand=' + Math.random()
 	});
