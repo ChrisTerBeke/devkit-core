eaf8be28f93321359f1bd5e325b2dadf148fb124
ChrisTerBeke <ctbeke@gmail.com>
2015-05-09 22:49:24 +0200

    added formide auth header, basic upload functionality

--- app/components/headers/devkit-printr-header-upload/component.js
+++ app/components/headers/devkit-printr-header-upload/component.js
@@ -6,27 +6,64 @@ var request			= require('request');
 var FormideUploadController = function($scope, $rootScope) {
 	
 	$scope.run = function() {
-		$scope.checkSession();
-		$scope.compress();
-		$scope.upload();	
+		if($scope.hasSession()) {
+			$scope.compressAndUpload();	
+		}
+		else {
+			alert('Not logged in!');
+		}
 	};
 	
-	$scope.checkSession = function() {
-		
+	$scope.hasSession = function() {
+		return window.localStorage.access_token !== undefined;
 	};
 	
-	$scope.compress = function() {
+	$scope.compressAndUpload = function() {
 		
-		console.log($scope.$parent.project);
-		
-		var zip = fs.createWriteStream(TEMP_FILENAME);
+		var projectDir = localStorage.getItem('project_dir');
+		var zipFile = projectDir + '/app.zip';
+		var zip = fs.createWriteStream(zipFile);
 		var archive = archiver('zip');
 		
+		archive.pipe(zip);
 		
-	};
-	
-	$scope.upload = function() {
+		archive.bulk([{
+			expand: true,
+			cwd: projectDir,
+			src: ['**']
+		}]);
 		
+		archive.finalize(function(err, bytes) {
+			if(err) {
+				console.log(err);
+			}
+		});
+		
+		zip.on('close', function() {
+			console.log('Uploading app...');
+			
+			var r = request({
+				url: 'https://api2.formide.com/apps/upload?access_token=' + window.localStorage.access_token,
+				method: 'post',
+				strictSSL: false
+			}, function(err, httpResponse, body) {
+				if (err) return console.log(err);
+				var response = JSON.parse(body);
+				if(response.success) {
+					console.log('upload ok');
+					// do something
+				}
+				else {
+					console.log('upload failed', response);
+					// do something
+				}
+			});
+			
+			var form = r.form();
+			form.append('version', '1.0.0'); // TODO: get version from manifest and auto update after uploading?
+			form.append('app_id', ''); // TODO: get app id from manifest
+			form.append('app_file', fs.createReadStream(zipFile, {filename: 'app.zip', contentType: 'application/zip'}));
+		});
 	};
 };
 
--- app_example/app.js
+++ app_example/app.js
@@ -18,30 +18,17 @@ loadModule('markdown', 		'widget',	'./core/components/widgets/devkit-widget-mark
 
 // APP
 // editors
-<<<<<<< HEAD:app/app.js
-loadModule('manifest', 		'editor',	'./app/components/editors/devkit-printr-editor-manifest/');
-loadModule('viewer', 		'editor',	'./app/components/editors/devkit-printr-editor-viewer/', ['printr.viewer']);
-
-// headers
-loadModule('upload', 		'header',	'./app/components/headers/devkit-printr-header-upload/');
-=======
 loadModule('manifest', 		'editor',	'./app_example/components/editors/devkit-homey-editor-manifest/');
 
 // headers
 loadModule('auth', 			'header',	'./app_example/components/headers/devkit-homey-header-auth/');
 loadModule('title', 		'header',	'./app_example/components/headers/devkit-homey-header-title/');
->>>>>>> 2e586e85ee5ee253653caa8ffc8ca699eb9202d6:app_example/app.js
 
 // widgets
 // nope..
 
 // themes
-<<<<<<< HEAD:app/app.js
-loadModule('custom_icons',	'theme',	'./app/components/themes/custom_icons/');
-loadModule('custom_icons',	'theme',	'./app/components/themes/formide/');
-=======
 loadModule('custom_icons',	'theme',	'./app_example/components/themes/custom_icons/');
->>>>>>> 2e586e85ee5ee253653caa8ffc8ca699eb9202d6:app_example/app.js
 
 /*
  * Use this area to define global settings for your app like the file editor config and devtools
