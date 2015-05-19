98376d36c98c52bd9b31bc0f8269b1f65dff2a13
Bouke Regnerus <bouke@regner.us>
2015-05-19 17:24:17 +0200

    Fixed Conflict

--- core/modules/services.js
+++ core/modules/services.js
@@ -6,9 +6,6 @@ module.services = angular.module('module.services', [
 	'sdk.stoplight',
 	'sdk.moduleload',
 	'sdk.popup',
-<<<<<<< HEAD
-	'sdk.project'
-=======
+	'sdk.project',
 	'sdk.menu'
->>>>>>> 040f1b91069b80e1e91d215905bc3bde4f631817
 ]);
