49a588283f8d928af2a6db340b3d25c7ead729f3
ChrisTerBeke <ctbeke@gmail.com>
2015-05-28 11:36:52 +0200

    removed uneccery update

--- controllers/EditorController.js
+++ controllers/EditorController.js
@@ -73,7 +73,6 @@ var EditorController = function($rootScope, $scope, $file, $project, $rootScope,
 
 	$rootScope.$on('menu.file-save', function(){
 		$file.save($scope.active);
-		$scope.update();
 	});
 }
 
